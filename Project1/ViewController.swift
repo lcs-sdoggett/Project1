2a2ed249f0660ec6b4acb0c03e62ea5d9ab4767f
Scott Doggett <sdoggett@lcs.on.ca>
2020-04-08 12:42:42 -0400

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Doggett, Scott on 2020-04-08.
+//  Copyright © 2020 Doggett, Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
